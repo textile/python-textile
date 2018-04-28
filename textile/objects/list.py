78639c2fb18226128c63d72f2c70365979ba2427
Dennis Burke <dennisburke@prodigy.net>
2018-04-28 11:44:28 -0400

    smoother way to handle sublists.

--- textile/objects/list.py
+++ textile/objects/list.py
@@ -8,7 +8,7 @@ class List(object):
         self.indent_level = indent_level
         self.items = []
 
-    def add_item(self, tag, content, attributes={}, sublist=False):
+    def add_item(self, tag, content, attributes={}):
         item = ListItem(tag, content, attributes)
         if type(content) is List:
             # if we are nesting lists, pop off the content of the most-recently
