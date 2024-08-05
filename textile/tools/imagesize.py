db58e41ab37a3f0a158d4ace15a51dbbbfcf665e
Dennis Burke <dennisburke@prodigy.net>
2024-08-05 14:23:05 -0400

    setting this back to what it was

--- textile/tools/imagesize.py
+++ textile/tools/imagesize.py
@@ -1,6 +1,3 @@
-from urllib.request import urlopen
-
-
 def getimagesize(url):
     """
     Attempts to determine an image's width and height, and returns a tuple,
@@ -13,6 +10,8 @@ def getimagesize(url):
         from PIL import ImageFile
     except ImportError:
         return ''
+
+    from urllib.request import urlopen
 
     try:
         p = ImageFile.Parser()
