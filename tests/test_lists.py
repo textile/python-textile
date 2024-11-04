5f2a113cf824370ca4089ba02d4fbb3a0a31d6bf
Dennis Burke <dennisburke@prodigy.net>
2024-11-04 13:55:44 -0500

    correct list usage in test

--- tests/test_lists.py
+++ tests/test_lists.py
@@ -11,16 +11,16 @@ def test_lists():
 
 def test_nested_list():
     lst = List('ol', indent_level=1)
-    lst('li', 'test')
+    lst.add_item('li', 'test')
     s = List('ol', indent_level=2)
     s.add_item('li', 'another one')
-    lst('li', s)
-    result = lst()
+    lst.add_item('li', s)
+    result = lst.process()
     expect = '\t<ol>\n\t\t<li>test\n\t\t<ol>\n\t\t\t<li>another one</li>\n\t\t</ol></li>\n\t</ol>'
     assert result == expect
 
     lst = List('ol', indent_level=1)
-    lst('li', 'test')
+    lst.add_item('li', 'test')
     s1 = List('ol', indent_level=2)
     s1.add_item('li', 'another one')
     s2 = List('ul', indent_level=3)
@@ -28,8 +28,8 @@ def test_nested_list():
     s2.add_item('li', 'point two')
     s1.add_item('li', s2)
     s1.add_item('li', 'moar item')
-    lst('li', s1)
-    result = lst()
+    lst.add_item('li', s1)
+    result = lst.process()
     expect = '\t<ol>\n\t\t<li>test\n\t\t<ol>\n\t\t\t<li>another one\n\t\t\t<ul>\n\t\t\t\t<li>point one</li>\n\t\t\t\t<li>point two</li>\n\t\t\t</ul></li>\n\t\t\t<li>moar item</li>\n\t\t</ol></li>\n\t</ol>'
     assert result == expect
 
