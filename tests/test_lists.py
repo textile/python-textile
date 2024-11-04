39f2de5c265cbbb9142824ffd5daf7113c94984d
Dennis Burke <dennisburke@prodigy.net>
2024-11-04 13:32:46 -0500

    fix E741 ambiguous variable name

--- tests/test_lists.py
+++ tests/test_lists.py
@@ -33,8 +33,8 @@ def test_nested_list():
     expect = '\t<ol>\n\t\t<li>test\n\t\t<ol>\n\t\t\t<li>another one\n\t\t\t<ul>\n\t\t\t\t<li>point one</li>\n\t\t\t\t<li>point two</li>\n\t\t\t</ul></li>\n\t\t\t<li>moar item</li>\n\t\t</ol></li>\n\t</ol>'
     assert result == expect
 
-    l = List('ol', indent_level=1)
-    l.add_item('li', 'test')
+    lst = List('ol', indent_level=1)
+    lst.add_item('li', 'test')
     s1 = List('ol', indent_level=2)
     s1.add_item('li', 'another one')
     s2 = List('ul', indent_level=3)
@@ -42,7 +42,7 @@ def test_nested_list():
     s2.add_item('li', 'point two')
     s1.add_item('li', s2)
     s1.add_item('li', 'moar item')
-    l.add_item('li', s1)
-    result = l.process()
+    lst.add_item('li', s1)
+    result = lst.process()
     expect = '\t<ol>\n\t\t<li>test\n\t\t<ol>\n\t\t\t<li>another one\n\t\t\t<ul>\n\t\t\t\t<li>point one</li>\n\t\t\t\t<li>point two</li>\n\t\t\t</ul></li>\n\t\t\t<li>moar item</li>\n\t\t</ol></li>\n\t</ol>'
     assert result == expect
